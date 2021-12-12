2950d85bccf425a9a9cf9d43c73a40067d6d086d
gukaifeng <github@gukaifeng.me>
2021-12-12 11:52:09 +0800

    the code for chapter 12

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+/*
+author: gukaifeng
+email: 892859816@qq.com
+github: https://github.com/gukaifeng/minigrep/
+blog: https://gukaifeng.cn/
+*/
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Applecation error: {}", e);
+        process::exit(1);
+    }
 }
